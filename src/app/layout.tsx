f1966aed9ecb8a7417c8d101ca027ee60b0fb380
EsleySaab <esleysantana.sa@gmail.com>
2025-02-18 19:09:54 -0300

    feat: finishing up the Home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import "./globals.css"
+import './globals.css'
 
-import type { Metadata } from "next"
-import { Montserrat, Oxanium } from "next/font/google"
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
 
 export const metadata: Metadata = {
-  title: "Devstage",
+  title: 'Devstage',
 }
 
 const oxanium = Oxanium({
-  weight: ["500", "600"],
-  subsets: ["latin"],
-  variable: "--font-oxanium",
+  weight: ['500', '600'],
+  subsets: ['latin'],
+  variable: '--font-oxanium',
 })
 
 const montserrat = Montserrat({
-  weight: ["400", "600"],
-  subsets: ["latin"],
-  variable: "--font-montserrat",
+  weight: ['400', '600'],
+  subsets: ['latin'],
+  variable: '--font-montserrat',
 })
 
 export default function RootLayout({
@@ -26,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
-      <body className="bg-gray-900 text-gray-100 antialiased">{children}</body>
+      <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top md:bg-right-top">
+        {children}
+      </body>
     </html>
   )
 }
